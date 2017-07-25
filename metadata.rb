9c83c16b234351146e72b2590ab474aa05524be5
Ștefan Rusu <saltwaterc@gmail.com>
2017-07-25 12:54:58 +0100

    supports doesn't expect an array

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ version '1.0.0'
 source_url 'https://github.com/SaltwaterC/remote_apt'
 issues_url 'https://github.com/SaltwaterC/remote_apt/issues'
 chef_version '>= 12.5'
-supports %w[debian ubuntu]
+%w[debian ubuntu].each do |platform|
+  supports platform
+end
 license 'MIT'
 description 'Installs remotely hosted apt packages'
